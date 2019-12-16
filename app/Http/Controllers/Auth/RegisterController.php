afb4923babf131a64cc06abe0e6c9b83b55570c2
Carlos <carlos_2017_1@hotmail.com>
2019-12-16 00:33:55 -0500

    Registro de cliente

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,8 +69,12 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'nombre' => $data['nombre'],
+            'apellidos' => $data['apellidos'],
+            'provincia' => $data['provincia'],
+            'telefono' => $data['telefono'],
+            'fechaNacimiento' => $data['fechaNacimiento'],
+            //'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
     }
