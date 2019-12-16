fd6c11b40a98d6f89ee2af3dac8d3a02ce902cf2
Carlos <carlos_2017_1@hotmail.com>
2019-12-16 02:06:40 -0500

    Registro de cliente exitoso

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'nombre' => ['required', 'string', 'max:255'],
             'apellidos' => ['required', 'string', 'max:255'],
             'provincia' => ['required', 'string', 'max:255'],
-            'telefono' => ['required', 'integer', 'min:9', 'max:9'],
+            'telefono' => ['required', 'integer'],
             'fechaNacimiento' => ['required', 'date'],
             //'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
